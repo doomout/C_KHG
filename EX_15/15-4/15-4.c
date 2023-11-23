4234e6ea2b19b92b035a06a2d69db8147f73298e
khg <winquake3@hanmail.net>
2023-11-24 05:44:32 +0900

    15-1. 이중 포인터와 배열 포인터까지

---
+++ EX_15/15-4/15-4.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int ary[5];
+
+	printf("    ary의 값 : %u\t", ary);
+	printf(" ary의 주소 : %u\n", &ary);
+	printf("     ary + 1 : %u\t", ary + 1);
+	printf("   &ary + 1 : %u\n", &ary + 1);
+
+	return 0;
+}
