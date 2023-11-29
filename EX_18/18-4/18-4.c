3dc87fd74185fa289595b1b8ecddc4fc62ba9f0c
khg <winquake3@hanmail.net>
2023-11-30 02:04:22 +0900

    18-1. 파일 열기와 입출력

---
+++ EX_18/18-4/18-4.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int ch; //입력한 문자를 저장할 변수
+
+	while (1)
+	{
+		ch = getchar(); //키보드에서 문자 입력
+		if (ch == EOF) //컨트롤 + z 로 입력 종료
+		{
+			break;
+		}
+		putchar(ch); //화면에 문자 출력
+	}
+
+	return 0;
+}
